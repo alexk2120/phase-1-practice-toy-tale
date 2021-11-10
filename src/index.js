ca5cee3307457b9ee446868c4a33e7e9007c406f
alexander.Khatamov <alexk2120@gmail.com>
2021-11-10 09:22:10 -0500

    commit tree b533cfd307964af0e06defb020f157082cae07c8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+
 let addToy = false;
+// oneToyName.textContent = oneToy.name
+// const oneToyImg = document.createElement('img')
+// oneToyonTheDom.append(oneToyName)
+// oneToyonTheDom.append(oneToyImg)
+
+
+
+//   <div class="card">
+//   <h2>Woody</h2>
+//   <img src="[toy_image_url]" class="toy-avatar" />
+//   <p>4 Likes </p>
+//   <button class="like-btn" id="[toy_id]">Like <3</button>
+ 
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +27,41 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+
+fetch('http://localhost:3000/toys')
+.then( res => res.json())
+.then(fetchData =>{ theStateOfOurFetchData = fetchData
+
+let oneToy = theStateOfOurFetchData[0]
+
+const toyCollectionDiv = document.querySelector('#toy-collection')
+  
+const oneToyonTheDom = document.createElement("div")
+oneToyonTheDom.setAttribute('class', 'card');
+
+
+oneToyonTheDom.innerHTML =
+ `<h2> ${oneToy.name}</h2>
+  <img src='${oneToy.image}' class="toy-avatar" />
+  <p> ${oneToy.likes} Likes </p>
+  <button class="like-btn" id='${oneToy.id}'>Like <3</button>`
+
+ 
+  
+
+
+toyCollectionDiv.append(oneToyonTheDom)
+
+const oneToyonTheDomLikeBtn = document.querySelector('.like-btn')
+
+oneToyonTheDomLikeBtn.addEventListener('click', () =>{ 
+
+let likesfromOneToyOnTheDom = ++oneToy.likes
+
+pTagOneToyOnTheDom = oneToyonTheDom.querySelector('p')
+
+pTagOneToyOnTheDom.innertext = `${likesfromOneToyOnTheDom} Likes`}
+
+)}
+)})
